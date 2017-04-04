2ba7dee1473dc4c909cadf253316da7d35c55db9
mbartzokas <m.bartzokas@gmail.com>
2017-04-04 20:28:15 +0200

    2.5

--- survey/2.5.py
+++ survey/2.5.py
@@ -1,10 +1,15 @@
 import Pmf
+import math
 
 def PmfMean(pmf):
-    return sum([pi * xi for pi,xi in pmf.GetDict().iteritems()])
+    return sum([pi * xi for xi, pi in pmf.GetDict().iteritems()])
 
 def PmfVar(pmf):
+    m = PmfMean(pmf)
+    return sum([pi * math.pow(xi - m, 2) for xi, pi in pmf.GetDict().iteritems()])
 
 pmf = Pmf.MakePmfFromDict({3: 4, 4: 1, 7:1, 8:2})
 
 print "mean is", PmfMean(pmf)
+print "variance is", PmfVar(pmf)
+print "standard deviation is", math.sqrt(PmfVar(pmf))
