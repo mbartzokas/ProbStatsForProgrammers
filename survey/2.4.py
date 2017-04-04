c53cc2a836a22ba1bb1ee8961b601324a43b642a
mbartzokas <m.bartzokas@gmail.com>
2017-04-04 19:25:03 +0200

    remaining lifetimes function

--- survey/2.4.py
+++ survey/2.4.py
@@ -1,4 +1,21 @@
 import Pmf
+import matplotlib.pyplot as pyplot
 
 def RemainingLifetimes(pmfLifetimes, age):
-    pass
+    pmf = pmfLifetimes.Copy("Remaining lifetimes")
+    for lifetime in pmf.Values():
+       freq = pmf.GetDict().pop(lifetime)
+       remainingLifetime = lifetime - age
+       if remainingLifetime > 0:
+           pmf.GetDict()[remainingLifetime] = freq
+    return pmf
+
+histLifetimes = Pmf.MakeHistFromDict({70: '10', 65: '8', 67:'9', 55: '5', 35: '1', 80: '2'}, "Lifetime frequencies")
+# vals, freqs = histLifetimes.Render()
+# pyplot.bar(vals, freqs)
+# pyplot.show()
+
+remainingLifetimes = RemainingLifetimes(histLifetimes, 47)
+vals, freqs = remainingLifetimes.Render()
+pyplot.bar(vals, freqs)
+pyplot.show()
