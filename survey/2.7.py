e22dddf59ee2c1e03f649e7212c8d32ac6375cd4
mbartzokas <m.bartzokas@gmail.com>
2017-04-14 11:07:46 +0200

    2.7 first solution better

--- survey/2.7.py
+++ survey/2.7.py
@@ -8,18 +8,25 @@ def firstBabiesPFM():
     return Pmf.MakePmfFromList(firsts.lengths, "First Babies")
 
 
+def generateFakeCohortAsDict(pmf, size):
+    return {length: int(size * pmf.Prob(length)) for length in pmf.Values()}
+
 def firstAlgorithm(pmf):
     # 1
-    lengthsToCounts = {length: int(1000 * pmf.Prob(length)) for length in pmf.Values()}
-    print 'lengthsToCounts', lengthsToCounts
+    cohort = generateFakeCohortAsDict(pmf, 1000)
+    print 'cohort', cohort
     # 2
-    gtEq39 = {lengthToCount[0]: 0 if lengthToCount[0] < 39 else lengthToCount[1] for lengthToCount in
-              lengthsToCounts.items()}
-    print 'lengthsToCountGtEq39', gtEq39
+    cohortGtEq39 = {lengthToCount[0]: 0 if lengthToCount[0] < 39 else lengthToCount[1] for lengthToCount in
+                    cohort.items()}
+    print 'cohort with lengths >= 39', cohortGtEq39
     # 3
-    conditionalPmf = Pmf.MakePmfFromDict(gtEq39)
+    conditionalPmf = Pmf.MakePmfFromDict(cohortGtEq39)
     # 4
     print 'PMF(39): {0} \nConditional PMF(39): {1}'.format(pmf.Prob(39), conditionalPmf.Prob(39))
+
+
+# def moreEfficientAlgorithm(pmf):
+# 1
 
 
 firstAlgorithm(firstBabiesPFM())
